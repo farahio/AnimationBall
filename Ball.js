00996e752a0ee7c3cb3a5a7ac05ed891acccae2f
Farahnaz Heydari <f.heydari@septaco.org>
2019-04-11 11:03:00 +0430

    panRespanser

---
+++ Ball.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View, StyleSheet,PanResponder,Animated} from 'react-native';
+
+class Ball extends Component {
+    constructor(props) {
+        super(props);
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+           onStartShouldSetPanResponder: () => true,
+           onPanResponderMove: (event, gesture) => {
+              position.setValue({ x: gesture.dx, y: gesture.dy });
+           }
+        });
+        this.state = { panResponder, position };
+     }
+
+   render() {
+    let handles = this.state.panResponder.panHandlers;
+      return (
+         <Animated.View  style={[styles.ball, this.state.position.getLayout()]}
+         {...handles}
+          />
+      );
+   }
+}
+
+const styles = StyleSheet.create({
+   ball: {
+      height: 80,
+      width: 80,
+      borderColor: '#B31CBA',
+      borderRadius: 40,
+      borderWidth: 40
+   },
+});
+
+export default Ball;
